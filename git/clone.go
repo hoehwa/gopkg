cc3406720393578ab190d8a6bd2586106ac897de
mindulle <mindullestudio@gmail.com>
2024-01-09 02:32:31 +0900

    init git package

---
+++ git/clone.go
@@ -0,0 +1,36 @@
+package git
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func checkIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
+
+// info should be used to describe the example commands that are about to run.
+func info(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+func Clone(owner string, repo string, path string) {
+	// Clone the given repository to the given directory
+	targetURL := fmt.Sprintf("https://github.com/%s/%s", owner, repo)
+
+	info(fmt.Sprintf("git clone %s", targetURL))
+
+	_, err := git.PlainClone(path, false, &git.CloneOptions{
+		URL:      targetURL,
+		Progress: os.Stdout,
+	})
+
+	checkIfError(err)
+}
